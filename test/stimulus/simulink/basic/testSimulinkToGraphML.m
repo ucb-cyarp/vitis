3443a1557b77d6e8462cac393f284dd9bb454f28
Christopher Yarp <cyarp@eecs.berkeley.edu>
2018-07-01 22:57:47 -0700

    Adding test stimulus for testing Simulink GraphML importer.

---
+++ test/stimulus/simulink/basic/testSimulinkToGraphML.m
@@ -0,0 +1,5 @@
+simulink_to_graphml('graphml_test_cases', 'graphml_test_cases/with_comments', 'commented.graphml')
+simulink_to_graphml('graphml_test_cases', 'graphml_test_cases/non_input_drivers', 'non_input_drivers.graphml')
+simulink_to_graphml('graphml_test_cases', 'graphml_test_cases/nested_enabled_subsystem', 'enabled.graphml')
+simulink_to_graphml('graphml_test_cases', 'graphml_test_cases/nested_subsystem', 'nested.graphml')
+simulink_to_graphml('graphml_test_cases', 'graphml_test_cases/simple_subsystem', 'simple.graphml')
