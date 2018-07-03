8d4eaf8d2803c4f2b5c8e7019362dca95d6cdfd2
Christopher Yarp <cyarp@eecs.berkeley.edu>
2018-07-03 15:09:18 -0700

    Missed the refactoring of 2 constructors in Enabled Nodes

--- src/GraphCore/EnableNode.cpp
+++ src/GraphCore/EnableNode.cpp
@@ -6,7 +6,8 @@
 #include "Port.h"
 
 EnableNode::EnableNode() {
-
+    enablePort = Port(this, Port::PortType::ENABLE, 0); //Don't need to do this in init as a raw pointer is passed to the port
+    //However, any call to get a shared_ptr of the node or port need to be conducted after a shared pointer has returned
 }
 
 EnableNode::EnableNode(std::shared_ptr<SubSystem> parent) : Node(parent) {
--- src/GraphCore/EnabledSubSystem.cpp
+++ src/GraphCore/EnabledSubSystem.cpp
@@ -5,7 +5,8 @@
 #include "EnabledSubSystem.h"
 
 EnabledSubSystem::EnabledSubSystem() {
-
+    enablePort = Port(this, Port::PortType::ENABLE, 0); //Don't need to do this in init as a raw pointer is passed to the port
+    //However, any call to get a shared_ptr of the node or port need to be conducted after a shared pointer has returned
 }
 
 EnabledSubSystem::EnabledSubSystem(std::shared_ptr<SubSystem> parent) : SubSystem(parent) {
