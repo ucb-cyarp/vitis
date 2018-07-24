b329d25e9dd15776a941abe7393e1abdcd8a7ee1
Christopher Yarp <cyarp@eecs.berkeley.edu>
2018-07-24 15:52:36 -0700

    New Simulink Stimulus with bugfix for VectorFan emit.  Also includes VectorFan direction.

--- test/stimulus/simulink/basic/testSimulinkToGraphML.m
+++ test/stimulus/simulink/basic/testSimulinkToGraphML.m
@@ -3,3 +3,5 @@ simulink_to_graphml('graphml_test_cases', 'graphml_test_cases/non_input_drivers'
 simulink_to_graphml('graphml_test_cases', 'graphml_test_cases/nested_enabled_subsystem', 'enabled.graphml')
 simulink_to_graphml('graphml_test_cases', 'graphml_test_cases/nested_subsystem', 'nested.graphml')
 simulink_to_graphml('graphml_test_cases', 'graphml_test_cases/simple_subsystem', 'simple.graphml')
+simulink_to_graphml('graphml_test_cases', 'graphml_test_cases/switch_subsytem', 'switch_subsytem.graphml')
+simulink_to_graphml('graphml_test_cases', 'graphml_test_cases/lut_subsystem', 'lut_subsystem.graphml')
