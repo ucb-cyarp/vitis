6aa8845dc021b8950820d56bc69b54a3005a92c3
cyarp <cyarp@eecs.berkeley.edu>
2018-07-30 18:23:23 -0700

    Adding GCC/gcov/lcov script

---
+++ scripts/bash/gccCoverage.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+startDir=`pwd`
+scriptDir=`dirname $0`
+
+rootDir="$scriptDir/../.."
+cd $rootDir
+mkdir buildCoverage
+cd buildCoverage
+cmake -DCMAKE_BUILD_TYPE=DEBUG ..
+make
+make test
+mkdir lcov
+cd lcov
+lcov --capture --directory ../CMakeFiles/ --output-file coverage.info
+lcov --remove coverage.info "/usr*" --output-file coverage_trimmed.info
+genhtml coverage_trimmed.info --output-directory covReport
+cp -r covReport $startDir/.
+
+cd $startDir
